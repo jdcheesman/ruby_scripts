56c5fc6aac3461ed09634e6704e1896c607151bf
Jim Cheesman <jdcheesman@gmail.com>
2013-04-29 13:25:19 +0200

    modifaciones, readme para no olvidar para que servia ese proyecto

--- log_entrada/log_parser.rb
+++ log_entrada/log_parser.rb
@@ -1,6 +1,15 @@
 require_relative 'ProcParser'
 require_relative 'OutputWriter'
 
+=begin
+
+Aplicacion para parsear el log de errores de alta de usuarios.
+
+Hay que pasar el data_munger primero para limpiar el log.
+
+=end
+
+
 def getfilename()
     time = Time.new
     "log_" + time.year.to_s + time.month.to_s + time.day.to_s + "-" +  time.hour.to_s + time.min.to_s + ".xlsx"
---
+++ log_entrada/readme.txt
@@ -0,0 +1,8 @@
+Application used to parse an export of the soid error table (<insert name here>).
+
+Application developed to provide José Calvo with information about LDAP errors.
+
+
+Application not used very often...
+
+
